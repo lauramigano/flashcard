237372b219d925c24ef17da0834c918f7d0e35a0
lauramigano <00001088146740sp@al.educacao.sp.gov.br>
2024-11-26 16:33:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'Um loop é uma estrutura de repetição que executa um bloco de código várias vezes'
+);
+
+criaCartao(
+    'Geografia',
+    'Qual é a montanha mais alta do mundo?',
+    'A montanha mais alta do mundo é o Monte Everest'
+);
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos para resolver um problema'
+);
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz adeus em inglês?',
+    'Adeus em inglês é "Goodbye"'
+);
